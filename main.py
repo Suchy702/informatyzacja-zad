9d7c05f0da63146447b2faec4214573d96f73352
Suchy702 <wojtek.suchy6@wp.pl>
2022-11-12 17:37:04 +0100

    change func

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-def is_squares_of_list(_list: list[int], squares: list[int]) -> bool:
-    return {i*i for i in _list} == set(squares)
+def are_squares_of_list_in_another_list(_list: list[int], squares: list[int]) -> bool:
+    return {i*i for i in _list}.issubset(set(squares))
+
 
