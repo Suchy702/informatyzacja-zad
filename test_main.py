c2f4c7c4b7ba2d93568e1b36cda77628615e9e7e
Suchy702 <wojtek.suchy6@wp.pl>
2022-11-12 17:49:11 +0100

    change tests

--- test_main.py
+++ test_main.py
@@ -1,39 +1,39 @@
-from main import is_squares_of_list
+from main import are_squares_of_list_in_another_list
 
 
 def test_empty_lists():
-    assert is_squares_of_list([], []) is True
+    assert are_squares_of_list_in_another_list([], []) is True
+
+
+def test_empty_list_squares_not():
+    assert are_squares_of_list_in_another_list([], [1, 2]) is True
 
 
 def test_one_element():
-    assert is_squares_of_list([1], [1]) is True
+    assert are_squares_of_list_in_another_list([1], [1]) is True
 
 
-def test_multiple_elements_unordered():
-    assert is_squares_of_list([1, 2, 1, 3, 2, 1, 2], [9, 1, 4, 9, 1]) is True
+def test_diff_numbers_in_squares():
+    assert are_squares_of_list_in_another_list([1, 2], [1, 2, 3, 4]) is True
 
 
-def test_wrong_same_list():
-    assert is_squares_of_list([1, 2, 3], [1, 2, 3]) is False
+def test_missing_square():
+    assert are_squares_of_list_in_another_list([1, 2, 3], [9, 1, 5, 5]) is False
 
 
-def test_list_empty_squares_not():
-    assert is_squares_of_list([], [1, 4, 9]) is False
+def test_unordered_and_duplicates():
+    assert are_squares_of_list_in_another_list([3, 5, 2], [25, 4, 4, 4, 9, 9]) is True
 
 
-def test_squares_empty_list_not():
-    assert is_squares_of_list([1, 2, 3], []) is False
+def test_negative_numbers():
+    assert are_squares_of_list_in_another_list([-2, -5, -1], [25, 1, 4]) is True
 
 
-def test_negative_numbers_in_list():
-    assert is_squares_of_list([-1, -2, -3], [1, 4, 9]) is True
+def test_negative_squares():
+    assert are_squares_of_list_in_another_list([-1, -2, -5], [-1, -4, -25]) is False
 
 
-def test_negative_numbers_in_squares():
-    assert is_squares_of_list([-1, -2, -3], [-1, -4, -9]) is False
-
-
-def test_1_000_000_elements_list():
+def test_1_000_000_elements_and_in_square_some_othera_numbers():
     _list = [i for i in range(1_000_000)]
-    squares = [i * i for i in _list]
-    assert is_squares_of_list(_list, squares) is True
+    squares = [i * i for i in range(1_000_000)] + [i for i in range(5, 1000, 6)]
+    assert are_squares_of_list_in_another_list(_list, squares) is True
